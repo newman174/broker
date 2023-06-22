7b5aa7c49d8d2f2e7294aff419feefb033184b40
Zach Morgan <zachmorgan97@gmail.com>
2023-06-22 15:02:52 -0700

    setup node.js backend

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Backend Setup
+- `cd` into `backend` directory
+- `npm install`
+- create a `.env` file
+- add `PORT=3001`
---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import 'dotenv/config';
+import morgan from 'morgan';
+
+import rootRoute from './routes/root.js';
+
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(express.json());
+
+app.use('/', rootRoute);
+
+const PORT = process.env.PORT;
+const server = app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
+
+export default server;
