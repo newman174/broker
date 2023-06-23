1607749fd78acd2d733b52b46e82fe8a67653796
vu-eric <eric16259@gmail.com>
2023-06-23 16:57:57 -0700

    pulled out swaggerMockValidatorFactory function from node_modules

--- server/src/services/sample_OAS.js
+++ server/src/services/sample_OAS.js
@@ -1,69 +1,67 @@
 export default {
-  "openapi":"3.0.2",
-  "info": {
-    "title": "user_service_api",
-    "version": "1"
+  openapi: "3.0.2",
+  info: {
+    title: "user_service_api",
+    version: "1",
   },
-  "servers": [
-    {"url":"https://api.server.test/v1"}
-  ],
-  "paths": {
+  servers: [{ url: "https://api.server.test/v1" }],
+  paths: {
     "/": {
-      "get": {
-        "responses": {
-          "200": {
-            "description": "Successful request",
-            "content": {
+      get: {
+        responses: {
+          200: {
+            description: "Successful request",
+            content: {
               "text/html; charset=utf-8": {
-                "schema": {
-                  "type": "string"
-                }
-              }
-            }
-          }
-        }
-      }
+                schema: {
+                  type: "string",
+                },
+              },
+            },
+          },
+        },
+      },
     },
     "/users/{id}": {
-      "get": {
-        "parameters": [
+      get: {
+        parameters: [
           {
-            "name": "id",
-            "in": "path",
-            "required": true,
-            "schema": {
-              "type": "integer"
+            name: "id",
+            in: "path",
+            required: true,
+            schema: {
+              type: "integer",
             },
-            "example": 1
-          }
+            example: 1,
+          },
         ],
-        "responses": {
-          "200": {
-            "description": "Successful request",
-            "content": {
+        responses: {
+          200: {
+            description: "Successful request",
+            content: {
               "application/json; charset=utf-8": {
-                "schema": {
-                  "type": "object",
-                  "properties": {
-                    "userId": {
-                      "type": "integer"
+                schema: {
+                  type: "object",
+                  properties: {
+                    userId: {
+                      type: "integer",
                     },
-                    "username": {
-                      "type": "string"
+                    username: {
+                      type: "integer",
                     },
-                    "touchedBy": {
-                      "type": "array",
-                      "items": {
-                        "type": "string"
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+                    touchedBy: {
+                      type: "array",
+                      items: {
+                        type: "string",
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 };
--- server/src/services/verification.js
+++ server/src/services/verification.js
@@ -9,71 +9,98 @@ verify(pact, OAS)
   if verification fails -> reject { pass: false, // the entire stdout for now}
 */
 
-import fs from 'node:fs';
-import path from 'node:path';
-import { exec } from 'child_process';
+import fs from "node:fs";
+import path from "node:path";
+import { exec } from "child_process";
+
+import { SwaggerMockValidatorFactory } from "../../node_modules/swagger-mock-validator/dist/swagger-mock-validator-factory.js";
 
 // contracts for development only. DELETE THIS WHEN verify() is called externally
-import samplePact from './sample_pact.js';
-import sampleOAS from './sample_OAS.js';
+import samplePact from "./sample_pact.js";
+import sampleOAS from "./sample_OAS.js";
 
 class Verifier {
   // takes both contracts as plain JS objects
   verify(pact, openAPISpec) {
-    const [ pactPath, OASPath ] = this.createFiles(pact, openAPISpec);
-    const initCommand           = `npx swagger-mock-validator ${OASPath} ${pactPath}`;
-  
+    const [pactPath, OASPath] = this.createFiles(pact, openAPISpec);
+    const initCommand = `npx swagger-mock-validator ${OASPath} ${pactPath}`;
+
     return new Promise((resolve, reject) => {
       exec(initCommand, (err, stdout, stderr) => {
         this.cleanUpFiles(pactPath, OASPath);
-  
+
         // THIS NEEDS TO BE TESTED ONCE WE HAVE AN ERROR HANDLING MIDDLEWHERE. SHOULD RESPOND WITH 500
         if (stderr) {
-          reject(new Error('Failed to execute swagger-mock-validator'));
+          reject(new Error("Failed to execute swagger-mock-validator"));
         }
-  
+
         if (!err) {
-          resolve({pass: true});
+          resolve({ pass: true });
         }
-    
+
         const verificationResult = {
           pass: false,
           stdout,
         };
-  
+
         reject(verificationResult);
       });
     });
   }
 
+  async verify2(pact, openAPISpec) {
+    const [pactPath, OASPath] = this.createFiles(pact, openAPISpec);
+
+    const swaggerMockValidator = SwaggerMockValidatorFactory.create();
+
+    const result = await swaggerMockValidator.validate({
+      mockPathOrUrl: pactPath,
+      specPathOrUrl: OASPath,
+    });
+
+    return [result.errors[0].mockDetails, result.errors[0].specDetails];
+  }
+
   createFiles(pact, openAPISpec) {
-    const pactPath = path.resolve(process.cwd(), 'src/services/verification_pact.json');
-    const OASPath  = path.resolve(process.cwd(), 'src/services/verification_OAS.json');
-  
-    const pactStream = fs.createWriteStream(pactPath)
-    pactStream.write(JSON.stringify(pact))
+    const pactPath = path.resolve(
+      process.cwd(),
+      "src/services/verification_pact.json"
+    );
+    const OASPath = path.resolve(
+      process.cwd(),
+      "src/services/verification_OAS.json"
+    );
+
+    const pactStream = fs.createWriteStream(pactPath);
+    pactStream.write(JSON.stringify(pact));
     pactStream.end();
-  
+
     const specStream = fs.createWriteStream(OASPath);
     specStream.write(JSON.stringify(openAPISpec));
     specStream.end();
-  
+
     return [pactPath, OASPath];
   }
 
   cleanUpFiles(pactPath, OASPath) {
     function onError(err) {
       if (err) {
-        console.log('Failed to cleanup a file. Error: ', err);
+        console.log("Failed to cleanup a file. Error: ", err);
       }
     }
-  
+
     fs.unlink(pactPath, onError);
     fs.unlink(OASPath, onError);
   }
 }
 
 // TESTING IN DEVELOPMENT ONLY - run `node verification.js`
-new Verifier().verify(samplePact, sampleOAS)
-  .then(result => console.log(result))
-  .catch(reason => console.log(reason));
+/*new Verifier()
+  .verify(samplePact, sampleOAS)
+  .then((result) => console.log(result))
+  .catch((reason) => console.log(reason));*/
+
+new Verifier()
+  .verify2(samplePact, sampleOAS)
+  .then((result) => console.log(result))
+  .catch((reason) => console.log(reason));
