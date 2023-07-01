a084099807d61c281d357e42df4c4bb3740344d6
Zach Morgan <zachmorgan97@gmail.com>
2023-06-30 20:26:26 -0700

    added tests for databaseClient.publishProviderSpec

--- server/src/test/setup.js
+++ server/src/test/setup.js
@@ -2,13 +2,8 @@ import { exec } from 'child_process';
 import knex from "../db/db";
 import server from '../app';
 
-beforeAll(async () => {
-  // teardown and rebuild the test database before running any tests
+afterAll(async () => {
   await exec('npm run resetdb:test');
-});
-
-afterAll(() => {
-  // close the database connection after all tests are complete
   knex.destroy();
   server.close();
 });
