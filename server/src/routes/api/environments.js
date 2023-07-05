a8ba2e77342eace7901793561094e592a41a5e39
Zach Morgan <zachmorgan97@gmail.com>
2023-07-04 20:37:54 -0700

    added boilerplate for PATCH /api/environments

--- server/src/routes/api/environments.js
+++ server/src/routes/api/environments.js
@@ -2,6 +2,14 @@ import express from "express";
 import db from "../../db/databaseClient.js";
 const router = express.Router();
 
+/*
+Creates a new deployment environment
+
+Request Body:
+{
+  environmentName: (string)
+}
+*/
 router.post('/', async (req, res) => {
   const { environmentName } = req.body;
 
@@ -14,6 +22,22 @@ router.post('/', async (req, res) => {
   const environmentRecord = await db.createEnvironment(environmentName);
 
   res.status(201).json(environmentRecord);
+});
+
+/*
+Updates the deployment status of a participant version in an environment
+
+Request Body:
+{
+  environmentName: (string)
+  participantName: (string)
+  participantVersion: (string)
+  deployed: (bool)
+}
+*/
+router.patch('/', async (req, res) => {
+  console.log(req.body);
+  res.status(200).json(req.body);
 });
 
 export default router
